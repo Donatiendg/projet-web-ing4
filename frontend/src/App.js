2fc9374fd97d16c85910387b767684a631640d60
Donatiendg <donatien.dg@outlook.com>
2022-11-18 16:25:57 +0100

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
+import axios from "axios";
+
+//RGAPI-98a2d96f-8a91-48cd-b6e2-3831658bb16a
+
+class Test extends Component {
+    state = {
+        name: "test"
+    };
+
+    handleChange = event => {
+        this.setState({name: event.target.value});
+    };
+
+  render() {
+      return(
+          <form>
+              <input type="text" onChange={this.handleChange}/>
+              <button>{this.state.name}</button>
+          </form>
+      );
+  }
+}
+
+class Forecast extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            forecast: null
+        };
+    }
+
+    callAPI = name => {
+        axios
+            .get(`?qqefafe`)
+            .then(({ data }) => {
+
+            })
+            .catch(console.error);
+    };
+}
 
 function App() {
   return (
     <div className="App">
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +59,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
